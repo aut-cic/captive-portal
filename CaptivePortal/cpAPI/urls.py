1f81abe300ebabb245b5487aa8b0efd908a15970
Parham Alvani <parham.alvani@gmail.com>
2017-07-13 19:23:51 +0430

    feat: update django application

--- CaptivePortal/cpAPI/urls.py
+++ CaptivePortal/cpAPI/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r"^info/$", views.index),
-    path(r"^userusage/$", views.user_usage),
-    path(r"^logout/$", views.logout),
+    path(r"info/", views.index),
+    path(r"userusage/", views.user_usage),
+    path(r"logout/", views.logout),
 ]
